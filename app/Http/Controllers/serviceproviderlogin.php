eb97f8603c00570308486f1891775db4f2b7912e
SaiyaraOishi <56593485+SaiyaraOishi@users.noreply.github.com>
2021-02-08 20:47:38 +0600

    Service Provider Dashboard

--- app/Http/Controllers/serviceproviderlogin.php
+++ app/Http/Controllers/serviceproviderlogin.php
@@ -2,29 +2,26 @@
 
 namespace App\Http\Controllers;
 
+use App\dreamwaver_booking;
 use Illuminate\Http\Request;
 use App\serviceproviderregister;
+
 
 class serviceproviderlogin extends Controller
 {
 
     function index(Request $request)
     {
-//        $request->validate(
-//            ['spemail'=>'required',
-//                'sppassword'=>'required']
-//        );
-//        return $request->input();
-//
-//    }
-//   function index(Request $request)
-//   {
-//       return "raisa";
+
         $email = serviceproviderregister::where('email','=',$request->spemail)->first();
         if($email)
         {
-            if($request->sppassword == $email->password)
-                return "success";
+            if($request->sppassword == $email->password) {
+                $allOrders = dreamwaver_booking::all();
+//                return redirect()->route('serviceProviderDashboard');
+                return view('layouts/serviceProviderDashboard')->with(['allOrders' => $allOrders]);
+            }
+
             else
                 return "invalid password";
 
