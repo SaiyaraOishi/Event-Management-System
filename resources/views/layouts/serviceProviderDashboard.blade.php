1434cce677b11473aa8e7d90c33c9481d2e59378
SaiyaraOishi <56593485+SaiyaraOishi@users.noreply.github.com>
2021-02-09 13:23:29 +0600

    Service Provider Dashboard with new parameter(service_provider_name)

--- resources/views/layouts/serviceProviderDashboard.blade.php
+++ resources/views/layouts/serviceProviderDashboard.blade.php
@@ -46,7 +46,7 @@
         <th scope="col">Phone Number</th>
         <th scope="col">Date</th>
         <th scope="col">Time</th>
-        <th scope="col">Service Type</th>
+        <th scope="col">Service Provider Name</th>
         <th scope="col">Created</th>
         <th scope="col">Updated</th>
         <th scope="col">Action</th>
@@ -61,7 +61,7 @@
                 <td scope="row">{{$order['phone']}}</td>
                 <td scope="row">{{$order['date']}}</td>
                 <td scope="row">{{$order['time']}}</td>
-                <td scope="row">{{$order['service_type']}}</td>
+                <td scope="row">{{$order['service_provider_name']}}</td>
                 <td scope="row">{{$order['created_at']}}</td>
                 <td scope="row">{{$order['updated_at']}}</td>
                 <td scope="row">
